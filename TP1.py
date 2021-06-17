232a8f45284f3d7bf1e7ebf905f9722b0a7e3996
Cquev <quevedomorato@gmail.com>
2021-06-16 23:55:51 -0300

    El código funciona y hace todo lo que tiene que hacer, hay que corregir cosas cosméticas y empezar a hacer el informe.

--- TP1.py
+++ TP1.py
@@ -75,17 +75,12 @@ def simulate_tasks_duration(graph_no_hire):
     # Sugerencia: recordar que la distribucion de la tarea i se encuentra en el
     # diccionario project_graph.d[i]
 
-    # Sacar el pass y reemplzar por el codigo
-    #pass
-    #VER SI USAMOS MÉTODO O DICCIONARIO DIRECTAMENTE
 
 def get_path_duration(path, tasks_times):
     pathduration = 0
-    path = graph_no_hire.get_paths()
     for task in path:
         pathduration += tasks_times[task]
     return pathduration
-
 
     # TODO TP1: Completar codigo
     # Dado un camino, representado por path y la secuencia de tareas, y la
@@ -95,7 +90,9 @@ def get_path_duration(path, tasks_times):
 
 def get_project_duration(graph_no_hire, tasks_times):
     projectduration = 0
-    for path in range(graph_no_hire.m):
+    paths = graph_no_hire.get_paths()
+    for i in range(graph_no_hire.m):
+        path = paths[i]
         path_duration = get_path_duration(path, tasks_times)
         if path_duration > projectduration:
             projectduration = path_duration
@@ -112,34 +109,43 @@ def get_project_duration(graph_no_hire, tasks_times):
 
 
 def simulate(n_sim, graph_no_hire):
-    simulations = []
+    vals = []
     for i in range(n_sim):
-        simulations.append(get_project_duration(graph_no_hire, tasks_times))
-    return simulations
+        tasks_times = simulate_tasks_duration(graph_no_hire)
+        vals.append(get_project_duration(graph_no_hire, tasks_times))
+    return vals
     # TODO TP1: Completar codigo
     # Esta funcion realiza n_sim simulaciones y analiza los resultados necesarios para el
     # arbol de decision.
     # Idealmente, puede devolver una lista con la duracion para cada una de las
     # simulaciones para su posterior analisis.
 
-    # Sacar el pass y reemplzar por el codigo
-    pass
 
-def get_prob_in_range(vals, a, b):
+
     # TODO TP1: Completar codigo
     # Funcion auxiliar para analisis de resultados y calculo de probabilidades.
     # Dada una muestra de valores vals, calcula la proporcion de x in vals tales que
     # a < x <= b.
 
     # Sacar el pass y reemplzar por el codigo
-    pass
+def get_prob_in_range(vals, a, b):
+    counter = 0
+    for time in vals:
+        if time > a and time < b:
+            counter += 1
+    return str(round((counter / len(vals))*100)) + "%"
+
+#def get_prob_in_range(vals, a, b):
+    # TODO TP1: Completar codigo
+    # Funcion auxiliar para analisis de resultados y calculo de probabilidades.
+    # Dada una muestra de valores vals, calcula la proporcion de x in vals tales que
+    # a < x <= b.
+
+
 
 def main():
-    # TODO TP1: Completar codigo
-
     # Fijamos la semilla, por reproducibilidad.
-    random.seed(0)
-
+    np.random.seed(0)
     # 300k por perder el deadline. Por ahora, 47.0
     # 150k por terminar en 40 o menos. Por ahora, mantenemos 40.0
     # Estos son los limites que nos importan.
@@ -148,27 +154,25 @@ def main():
 
     # Primer paso: consideramos el escenario sin contrataciones extra.
     graph_no_hire = ProjectGraph()
-
     print(graph_no_hire.dist_params[2])
-
-    # TODO: simular en este contexto!
-    TD = simulate_tasks_duration(graph_no_hire)
-    print(TD)
     SM = simulate(1000, graph_no_hire)
-    print(SM)
 
     # TODO: Analizar los resultados.
-
+    print(get_prob_in_range(SM, 0, incentive))
+    print(get_prob_in_range(SM, incentive, deadline))
+    print(get_prob_in_range(SM, deadline, 100))
     # Segundo paso: analizar en el contexto que se hace la contratacion.
     # Esto lo representamos modifiando la duracion de las tarea 2.
     graph_hire = graph_no_hire
 
     graph_hire.dist_params[2] = (6.0, 9.0, 13.0)
     print(graph_hire.dist_params[2])
-    print(graph_hire.dist_params[2][0], graph_hire.dist_params[2][1], graph_hire.dist_params[2][2])
 
+    SMH = simulate(1000, graph_hire)
     # TODO: simular en este contexto!
-
+    print(get_prob_in_range(SMH, 0, incentive))
+    print(get_prob_in_range(SMH, incentive, deadline))
+    print(get_prob_in_range(SMH, deadline, 100))
 
     # TODO: Analizar los resultados.
 
--- TP1Test.py
+++ TP1Test.py
@@ -78,11 +78,9 @@ def simulate_tasks_duration(graph_no_hire):
 
 def get_path_duration(path, tasks_times):
     pathduration = 0
-    path = graph_no_hire.get_paths()
     for task in path:
         pathduration += tasks_times[task]
     return pathduration
-
 
     # TODO TP1: Completar codigo
     # Dado un camino, representado por path y la secuencia de tareas, y la
@@ -92,7 +90,9 @@ def get_path_duration(path, tasks_times):
 
 def get_project_duration(graph_no_hire, tasks_times):
     projectduration = 0
-    for path in range(graph_no_hire.m):
+    paths = graph_no_hire.get_paths()
+    for i in range(graph_no_hire.m):
+        path = paths[i]
         path_duration = get_path_duration(path, tasks_times)
         if path_duration > projectduration:
             projectduration = path_duration
@@ -109,19 +109,31 @@ def get_project_duration(graph_no_hire, tasks_times):
 
 
 def simulate(n_sim, graph_no_hire):
-    TD = simulate_tasks_duration(graph_no_hire)
-    simulations = []
+    vals = []
     for i in range(n_sim):
-        simulations.append(get_project_duration(graph_no_hire, TD))
-    return simulations
+        tasks_times = simulate_tasks_duration(graph_no_hire)
+        vals.append(get_project_duration(graph_no_hire, tasks_times))
+    return vals
     # TODO TP1: Completar codigo
     # Esta funcion realiza n_sim simulaciones y analiza los resultados necesarios para el
     # arbol de decision.
     # Idealmente, puede devolver una lista con la duracion para cada una de las
     # simulaciones para su posterior analisis.
 
+
+
+    # TODO TP1: Completar codigo
+    # Funcion auxiliar para analisis de resultados y calculo de probabilidades.
+    # Dada una muestra de valores vals, calcula la proporcion de x in vals tales que
+    # a < x <= b.
+
     # Sacar el pass y reemplzar por el codigo
-    pass
+def get_prob_in_range(vals, a, b):
+    counter = 0
+    for time in vals:
+        if time > a and time < b:
+            counter += 1
+    return str(round((counter / len(vals))*100)) + "%"
 
 #def get_prob_in_range(vals, a, b):
     # TODO TP1: Completar codigo
@@ -132,8 +144,6 @@ def simulate(n_sim, graph_no_hire):
 
 
 def main():
-    # TODO TP1: Completar codigo
-
     # Fijamos la semilla, por reproducibilidad.
     random.seed(0)
 
@@ -145,29 +155,25 @@ def main():
 
     # Primer paso: consideramos el escenario sin contrataciones extra.
     graph_no_hire = ProjectGraph()
-
     print(graph_no_hire.dist_params[2])
-
-
-    # TODO: simular en este contexto!
-    TD = simulate_tasks_duration(graph_no_hire)
-    print(TD)
     SM = simulate(1000, graph_no_hire)
-    print(SM)
-
 
     # TODO: Analizar los resultados.
-
+    print(get_prob_in_range(SM, 0, incentive))
+    print(get_prob_in_range(SM, incentive, deadline))
+    print(get_prob_in_range(SM, deadline, 100))
     # Segundo paso: analizar en el contexto que se hace la contratacion.
     # Esto lo representamos modifiando la duracion de las tarea 2.
     graph_hire = graph_no_hire
 
     graph_hire.dist_params[2] = (6.0, 9.0, 13.0)
     print(graph_hire.dist_params[2])
-    print(graph_hire.dist_params[2][0], graph_hire.dist_params[2][1], graph_hire.dist_params[2][2])
 
+    SMH = simulate(1000, graph_hire)
     # TODO: simular en este contexto!
-
+    print(get_prob_in_range(SMH, 0, incentive))
+    print(get_prob_in_range(SMH, incentive, deadline))
+    print(get_prob_in_range(SMH, deadline, 100))
 
     # TODO: Analizar los resultados.
 
